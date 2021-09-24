641f75068d84cbf3eac283c0bb74a6d751986b9c
AbhijithS <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2021-09-24 17:58:10 +0530

    Create __init__.py

---
+++ VoiceChat_Song_bot/functions/__init__.py
@@ -0,0 +1,5 @@
+from VoiceChat_Song_bot.function.admins import admins
+from VoiceChat_Song_bot.function.admins import get
+from VoiceChat_Song_bot.function.admins import set
+
+__all__ = ["set", "get", "admins"]
