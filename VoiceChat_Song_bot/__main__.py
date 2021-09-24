86dbdc45fea6bc14706d791a90cf0bc70515616b
AbhijithS <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2021-09-24 17:50:12 +0530

    Create __main__.py

---
+++ VoiceChat_Song_bot/__main__.py
@@ -0,0 +1,24 @@
+import requests
+from pyrogram import Client as Bot
+
+from VoiceChat_Song_bot.config import API_HASH
+from VoiceChat_Song_bot.config import API_ID
+from VoiceChat_Song_bot.config import BG_IMAGE
+from VoiceChat_Song_bot.config import BOT_TOKEN
+from VoiceChat_Song_bot.services.callsmusic import run
+
+response = requests.get(BG_IMAGE)
+file = open("./etc/foreground.png", "wb")
+file.write(response.content)
+file.close()
+
+bot = Bot(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="VoiceChat_Song_bot.modules"),
+)
+
+bot.start()
+run()
