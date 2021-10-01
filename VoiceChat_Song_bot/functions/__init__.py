faa3708d182f9233db2c8c1ab62cac714e59bbf2
AbhijithS <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2021-10-01 20:15:12 +0530

    Update __init__.py
    Small 🪲

--- VoiceChat_Song_bot/functions/__init__.py
+++ VoiceChat_Song_bot/functions/__init__.py
@@ -1,5 +1,5 @@
-from VoiceChat_Song_bot.function.admins import admins
-from VoiceChat_Song_bot.function.admins import get
 from VoiceChat_Song_bot.function.admins import set
+from VoiceChat_Song_bot.function.admins import get
+from VoiceChat_Song_bot.function.admins import admins
 
 __all__ = ["set", "get", "admins"]
