4a7d2a53933157ee96c857e02522d81b480131e9
AbhijithS <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2021-11-09 21:57:56 +0530

    Update __init__.py

--- VoiceChat_Song_bot/services/queues/__init__.py
+++ VoiceChat_Song_bot/services/queues/__init__.py
@@ -1,3 +1,3 @@
-from VoiceChat_Song_bot.services.queues.queues import clear, get, is_empty, put, task_done
+from VoiceChat_Song_bot.services.queues. import clear, get, is_empty, put, task_done
 
 __all__ = ["clear", "get", "is_empty", "put", "task_done"]
