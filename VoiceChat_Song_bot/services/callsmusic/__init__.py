46c568557c8e2dfde987f4e6dc2c737b9b75ee7c
AbhijithS <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2021-11-09 21:47:22 +0530

    Update __init__.py
    wait i need another try😑

--- VoiceChat_Song_bot/services/callsmusic/__init__.py
+++ VoiceChat_Song_bot/services/callsmusic/__init__.py
@@ -1,6 +1,2 @@
-from pyrogram import Client
-
-from VoiceChat_Song_bot import config
-
-client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
-run = client.run
+from callsmusic.queues import queues
+from callsmusic.callsmusic import pytgcalls, run
