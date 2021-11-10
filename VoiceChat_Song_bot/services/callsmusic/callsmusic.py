71fc95ddcce2542c34ed9e0405df96f566d426fd
AbhijithS <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2021-11-10 19:57:19 +0530

    Update callsmusic.py

--- VoiceChat_Song_bot/services/callsmusic/callsmusic.py
+++ VoiceChat_Song_bot/services/callsmusic/callsmusic.py
@@ -1,29 +1,21 @@
-from VoiceChat_Song_bot.services.queues import queues
 from pyrogram import Client
 from pytgcalls import PyTgCalls
-from pytgcalls.types import Update
-from pytgcalls.types.input_stream import InputAudioStream
-from config import API_HASH, API_ID, SESSION_NAME
 
+from VoiceChat_Song_bot.config import API_HASH, API_ID, SESSION_NAME
+from VoiceChat_Song_bot.services.callsmusic import queues
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
 pytgcalls = PyTgCalls(client)
 
 
 @pytgcalls.on_stream_end()
-async def on_stream_end(client: PyTgCalls, update: Update) -> None:
-    chat_id = update.chat_id
+def on_stream_end(chat_id: int) -> None:
     queues.task_done(chat_id)
 
     if queues.is_empty(chat_id):
-        await pytgcalls.leave_group_call(chat_id)
+        pytgcalls.leave_group_call(chat_id)
     else:
-        await pytgcalls.change_stream(
-            chat_id,
-            InputAudioStream(
-                queues.get(chat_id)["file"],
-            ),
-        )
+        pytgcalls.change_stream(chat_id, queues.get(chat_id)["file"])
 
 
-run = pytgcalls.start
+run = pytgcalls.run
