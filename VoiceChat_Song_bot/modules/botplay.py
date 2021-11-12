ca593b2c6694c872c963ea638b1f391498232b5f
AbhijithS <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2021-11-12 15:49:32 +0530

    Update botplay.py
    No 😂

--- VoiceChat_Song_bot/modules/botplay.py
+++ VoiceChat_Song_bot/modules/botplay.py
@@ -114,17 +114,6 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
     Image.alpha_composite(image5, image6).save("temp.png")
     img = Image.open("temp.png")
     img = Image.open("temp.png")
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("etc/font(1).otf", 32)
-    draw.text((205, 550), f"Title: {title}", (51, 215, 255), font=font)
-    draw.text((205, 590), f"Duration: {duration}", (255, 255, 255), font=font)
-    draw.text((205, 630), f"Views: {views}", (255, 255, 255), font=font)
-    draw.text(
-        (205, 670),
-        f"Added By: {requested_by}",
-        (255, 255, 255),
-        font=font,
-    )
     img.save("final.png")
     os.remove("temp.png")
     os.remove("background.png")
